38b65906de499305e4a5c3d076692e9d721ad9cc
Furkan Ertekin <furkanertekin0@gmail.com>
2022-12-17 17:03:36 +0300

    seri hesabi

---
+++ seri_hesabi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeriHesabi
+{
+    internal class Program
+    {
+        public static int Seri(int x)
+        {
+            int sonuc = 0;
+
+            for (int i = 0; i <=x; i++)
+            {
+                sonuc = sonuc + (i*i);
+            }
+            return sonuc;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hoşgeldiniz!");
+            Console.Write("Lütfen bir X değeri giriniz : ");
+            int x=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Seri(x));
+            Console.ReadLine();
+
+        }
+    }
+}
