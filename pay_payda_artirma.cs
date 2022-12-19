77c935a0dade9d34ff05272c0872cc19064aed91
Furkan Ertekin <furkanertekin0@gmail.com>
2022-12-19 19:49:44 +0300

    pay payda artirma

---
+++ pay_payda_artirma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dongu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pay = 1;
+            int payda = 2;
+
+            for (int i = 1; i < 9; i++)
+            {
+                Console.WriteLine(pay + "/" + payda);
+                pay++;
+                payda++;
+                if (i==5)
+                {
+                    pay++;
+                    payda++;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
