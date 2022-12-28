7b743bd799a679a5a28899adf4b4be2db69f6ee9
Furkan Ertekin <furkanertekin0@gmail.com>
2022-12-28 23:09:21 +0300

    dikdortgen

---
+++ dikdortgen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dikdortgen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir boy değeri giriniz : ");
+            int boy = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Lütfen bir en değeri giriniz : ");
+            int en = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Sonuç : ");
+
+            Console.WriteLine(" ");
+
+            for (int i = 1; i <= boy; i++)
+            {
+                for (int j = 1; j <= en; j++)
+                {
+                    if (i == 1 || i == boy)
+                        Console.Write("*");
+                    else
+                        if (j == 1 || j == en)
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }         
+            Console.ReadLine();
+        }
+    }
+}
